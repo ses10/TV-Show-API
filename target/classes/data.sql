ac82fedac0d1d9b8d58445e6e1bdb948838fdb3a
ses10 <sesma@live.com>
2018-07-23 20:41:32 -0400

    add the target folder

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into SHOW (ID,TITLE,DESCRIPTION,DURATION,ORIGINAL_AIR_DATE,RATING,KEYWORDS)
+values (1000, 'Malcom in the Middle', 'A sitcom', 30, '2/12/2003', 6, 'family,sitcom,comedy,mediocre');
+
+insert into SHOW (ID,TITLE,DESCRIPTION,DURATION,ORIGINAL_AIR_DATE,RATING,KEYWORDS)
+values (1002, 'Thats so Raven', 'A Disney sitcom', 30, '4/23/2001', 5, 'family,sitcom,comedy,mediocre');
+
+insert into SHOW (ID,TITLE,DESCRIPTION,DURATION,ORIGINAL_AIR_DATE,RATING,KEYWORDS)
+values (1003, '70s Show', 'A good sitcom', 30, '3/11/1998', 8, 'family,sitcom,comedy,good');
